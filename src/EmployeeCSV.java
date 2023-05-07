22b396e3ec700b3a9c1030cce6a64aa95f06f890
sukkieroha <lr.zjdongon@mmdc.mcl.edu.ph>
2023-05-07 17:38:29 +0800

    2nd commit

--- src/EmployeeCSV.java
+++ src/EmployeeCSV.java
@@ -10,10 +10,10 @@ public class EmployeeCSV {
 
         // Create list of employees
         List<Employee> employees = new ArrayList<>();
-        employees.add(new Employee("10001", "Jose", "Crisostomo", "123456789", "987654321", "567890123", "111222333", 200, 1500.00, 2000.00, 1000.00, 5, 10, 5));
-        employees.add(new Employee("10002", "Christian", "Mata", "234567890", "876543210", "678901234", "444555666", 150, 1500.00, 2000.00, 1000.00, 5, 10, 5));
-        employees.add(new Employee("003", "Brad", "San Jose", "345678901", "765432109", "789012345", "777888999", 100, 1500.00, 2000.00, 1000.00, 5, 10, 5));
-
+        employees.add(new Employee("10001", "Jose", "Crisostomo", "631052853464", "631052853464", "631052853464", "631052853464", 373.04, 1500.00, 1000, 1000.00, 5, 10, 5));
+        employees.add(new Employee("10002", "Christian", "Mata", "49-2959312-6", "824187961962", "631052853464", "444555666", 255.80, 1500.00, 800, 800, 5, 10, 5));
+        employees.add(new Employee("10003", "Brad", "San Jose", "40-2400714-1	","239192926939",	"672-474-690-000",	"210850209964", 255.80, 1500.00, 800, 800, 5, 10, 5));
+        employees.add(new Employee("10004","Anthony","Salcedo","26-9647608-3",	"126445315651",	"210-805-911-000",	"218002473454",362.05,1500.00,1000,1000,5,10,5));
         // Write employee details to CSV file
         String employeeFilename = "employees.csv";
         try (FileWriter writer = new FileWriter(employeeFilename)) {
